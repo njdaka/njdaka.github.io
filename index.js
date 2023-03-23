d29aa26bbf80df5ede6bc1a01c4620d6c4d63fd0
njdaka <85201030+njdaka@users.noreply.github.com>
2023-03-23 12:59:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed("multiple", {
+    strings: ["Front-end developer", "UI/UX Designer", "Photo editor", "Pentester"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+})
