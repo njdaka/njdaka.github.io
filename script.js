dd124839dd7ca288b35c774e976c2ab9480ebf5d
njdaka <85201030+njdaka@users.noreply.github.com>
2023-03-27 10:11:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple", {
-    strings: ["Front-end developer", "UI/UX Designer", "Photo editor", "Pentester", "Batman in the evening😁"],
+    strings: ["Front-end developer", "UI/UX Designer", "Photo editor", "Pentester", "Superhero in the evening😁"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
